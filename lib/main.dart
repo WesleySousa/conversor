650fdaad65a88ebb7458fb2585618f23d87c7d52
Wesley Silva <wesley@simplecodeti.com.br>
2020-05-26 21:15:06 -0300

    - melhorado id do app android e ios. - adicionado campos de Real, Dollar e Euro. - Adicionado funções de calculos de conversao de moeda.

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,17 @@ main() async {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Home(),
+    theme: ThemeData(
+      hintColor: Colors.amber,
+      primaryColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        focusedBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+        hintStyle: TextStyle(color: Colors.amber),
+      ),
+    ),
   ));
 }
 
@@ -18,6 +29,50 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dollarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double _dollar;
+  double _euro;
+
+  void _changeReal(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dollarController.text = (real / _dollar).toStringAsFixed(2);
+    euroController.text = (real / _euro).toStringAsFixed(2);
+  }
+
+  void _changeDollar(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dollar = double.parse(text);
+    realController.text = (dollar * this._dollar).toStringAsFixed(2);
+    euroController.text = (dollar * this._dollar / _euro).toStringAsFixed(2);
+  }
+
+  void _changeEuro(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text = (euro * this._euro).toStringAsFixed(2);
+    dollarController.text =
+        (euro * this._euro / this._dollar).toStringAsFixed(2);
+  }
+
+  void _clearAll() {
+    realController.text = '';
+    dollarController.text = '';
+    euroController.text = '';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,8 +105,28 @@ class _HomeState extends State<Home> {
                   ),
                 );
               } else {
-                return Container(
-                  color: Colors.green,
+                _dollar = snapshot.data['results']['currencies']['USD']['buy'];
+                _euro = snapshot.data['results']['currencies']['EUR']['buy'];
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(
+                        Icons.monetization_on,
+                        size: 150.0,
+                        color: Colors.amber,
+                      ),
+                      buildTextField(
+                          'Reaís', 'R\$ ', realController, _changeReal),
+                      Divider(),
+                      buildTextField(
+                          'Dólares', 'US\$ ', dollarController, _changeDollar),
+                      Divider(),
+                      buildTextField(
+                          'Euros', '€ ', euroController, _changeEuro),
+                    ],
+                  ),
                 );
               }
           }
@@ -59,6 +134,23 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+}
+
+Widget buildTextField(String label, String prefix,
+    TextEditingController controller, Function change) {
+  return TextField(
+    controller: controller,
+    onChanged: change,
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
+    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+    decoration: InputDecoration(
+      labelText: label,
+      labelStyle: TextStyle(color: Colors.amber, fontSize: 25.0),
+      border: OutlineInputBorder(),
+      prefixText: prefix,
+      prefixStyle: TextStyle(color: Colors.amber, fontSize: 25.0),
+    ),
+  );
 }
 
 Future<Map> getData() async {
